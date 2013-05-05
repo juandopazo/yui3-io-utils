c04ac13cd27e93aa212a2cd2ea8fa1c028e15aab
Juan Ignacio Dopazo <dopazo.juan@gmail.com>
2013-05-05 19:30:22 -0300

    Publish API Docs

---
+++ api/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "io"
+    ],
+    "modules": [
+        "io-extras"
+    ],
+    "allModules": [
+        {
+            "displayName": "io-extras",
+            "name": "io-extras",
+            "description": "Promise based methods for performing IO requests."
+        }
+    ]
+} };
+});
